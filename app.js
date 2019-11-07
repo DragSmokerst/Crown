a1fc96bf528c77014766537e7294d249b979b936
DragSmokerst <dragsmokerst@gmail.com>
2019-11-07 21:59:21 +0700

    Add standard file

---
+++ app.js
@@ -0,0 +1,11 @@
+function startGame(){
+
+}
+var myGameArea = {
+    canvas: document.createElement("canvas"),
+    start: function(){
+        this.canvas.height = 1000;
+        this.canvas.width = 1000;
+        this.context
+    }
+}
